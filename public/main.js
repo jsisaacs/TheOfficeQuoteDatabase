0419a113c5eee2f80ccbf665f3fb2e519f6f1c8b
Josh Isaacson <jsisaacs@iu.edu>
2017-10-14 18:47:03 -0400

    finished project

---
+++ public/main.js
@@ -0,0 +1,35 @@
+
+/* globals fetch */
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Dwight Schrute',
+      'quote': 'How would I describe myself? Three words: hardworking, alpha male, jackhammer, merciless, insatiable.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Dwight Schrute'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+MongoClient.connect('mongodb://Josh:hockey12321.@ds063892.mlab.com:63892/office-quotes', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, () => {
+		console.log('listening on 3000')
+	})
+})
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/')
+	})
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Michael Scott'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A Michael Scott quote got deleted')
+  })
+})
